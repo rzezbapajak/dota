2e284c98b12c52206baf90ce307615b7d572aa2d
janjanek <jas.star@wp.pl>
2020-10-01 18:45:56 +0200

    backend init

---
+++ src/main/java/pl/jstar/dota/DotaApplication.java
@@ -0,0 +1,13 @@
+package pl.jstar.dota;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DotaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DotaApplication.class, args);
+    }
+
+}
